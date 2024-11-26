39b9712b3bc479e09f8454e6404defb49c2c0498
Tushar Khaire <tusharkhaire099@gmail.com>
2024-11-26 21:17:52 +0530

    Add project files.

---
+++ DotNetTestSaveImageToDB/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace DotNetTestSaveImageToDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProduct prod;
+        public ProductController(IProduct _prod)
+        {
+            this.prod = _prod;
+        }
+        [HttpPost("SaveProduct")]
+        public async Task<IActionResult> SaveProduct(Product product)
+        {
+            var isSave=await prod.SaveProduct(product);
+            return Ok();
+        }
+    }
+}
